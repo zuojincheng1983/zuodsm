792b0e798c02e381bf91d21231a76338bbebf116
zuojincheng1983 <lf_zuo@163.com>
2019-04-23 19:46:17 +0800

    播放器完成

---
+++ player/js/playlist.js
@@ -0,0 +1,24 @@
+// 新版chrome禁止了自动b播放
+$('.playlist li').click(function () {
+    var cover = './img/' + $(this).index() + '.jpg';
+    var music = './mp3/' + $(this).index() + '.mp3';
+    $('.audio .cover').attr('src', cover);
+    $('#mypalyer').attr('src', music);
+});
+
+
+// 注册播放列表中删除按钮的事件
+$('.del').click(function () {
+    $(this).parent().parent().css('display', 'none');
+    var cover = './img/no.jpg';
+    var music = '';
+    $('.audio .cover').attr('src', cover);
+    $('#mypalyer').attr('src', music);
+
+    // 阻止冒泡
+    if (window.event) {
+        event.cancelBubble = true;
+    } else {
+        event.stopPropagation();
+    }
+});
